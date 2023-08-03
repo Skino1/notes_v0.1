62f64d5bb944d397074c6d60c90b15a6180659ab
Skino1 <filgooten@gmail.com>
2023-08-03 20:01:19 +0300

    update

--- app/assets/script/menu.js
+++ app/assets/script/menu.js
@@ -1,24 +1,31 @@
 const body = document.querySelector("#body_container");
 
 const menuBtn = document.querySelector("#menu_button");
-const menuElements = document.querySelectorAll(".menu_element");
+const menuElements = Array.from(document.querySelectorAll(".menu_element"));
 const menuIcons = document.querySelectorAll("#menu_icon");
 const menuElTitle = document.querySelectorAll("#menu_element_title");
-const menu = document.querySelector("#menu");
+const menu = Array.from(document.querySelectorAll("#menu"));
+const menuDesktop = document.querySelector("#menu_desktop")
 
 const createNewElement = document.querySelector(".create_new")
 
 const filterIcon = document.querySelector("#filters_mobile");
 const filterMenu = document.querySelector("#filters_menu_mobile");
 
-
+const noteElement = menuElements.filter(
+  (element) => element.dataset.type === "menu_notes"
+);
+const listElement = menuElements.filter(
+  (element) => element.dataset.type === "menu_lists"
+);
 let typeOfNotes = 'note';
 
-menuElements[0].classList.add("bg-slate-700");
+noteElement.forEach(element => {
+  element.classList.add("bg-slate-700");
+});
 
 menuBtn.addEventListener("click", function () {
-    body.classList.toggle("grid-cols-[0.20fr_1fr]")
-    body.classList.toggle("grid-cols-[0.05fr_1fr]")
+    body.classList.replace("grid-cols-[0.20fr_1fr]", "grid-cols-[0.05fr_1fr]");
   for (let i = 0; i < menuElTitle.length; i++) {
     const element = menuElTitle[i];
       element.classList.toggle("hidden");
@@ -31,27 +38,38 @@ menuBtn.addEventListener("click", function () {
       element.classList.replace("md:p-3", "md:p-5");
     }
   }
-    menu.classList.toggle("w-20");
+    menuDesktop.classList.toggle("w-20");
 });
 
-menu.addEventListener("click", function (event) {
-  if (event.target.dataset.type) {
-    const type = event.target.dataset.type;
-    if (type == "menu_notes") {
-      menuElements[0].classList.add("bg-slate-700");
-      menuElements[1].classList.add('hover:bg-slate-800')
-      menuElements[0].classList.remove("hover:bg-slate-800");
-      menuElements[1].classList.remove("bg-slate-700");
-      typeOfNotes = 'note'
+menu.forEach(element => {
+  element.addEventListener("click", function (event) {
+    if (event.target.dataset.type) {
+      const type = event.target.dataset.type;
+      if (type == "menu_notes") {
+        noteElement.forEach(element => {
+          element.classList.add("bg-slate-700");
+          element.classList.remove("hover:bg-slate-800");
+        });
+        listElement.forEach(element => {
+          element.classList.add("hover:bg-slate-800"); 
+          element.classList.remove("bg-slate-700");
+        });
+        typeOfNotes = 'note'
+      }
+      if (type == "menu_lists") {
+        noteElement.forEach(element => {
+          element.classList.remove("bg-slate-700");
+          element.classList.add("hover:bg-slate-800");
+        });
+        listElement.forEach(element => {
+          element.classList.add("bg-slate-700");
+          element.classList.remove("hover:bg-slate-800");
+        });
+        typeOfNotes = 'list'
+      }
     }
-    if (type == "menu_lists") {
-      menuElements[0].classList.remove("bg-slate-700");
-      menuElements[1].classList.add("bg-slate-700");
-      menuElements[0].classList.add("hover:bg-slate-800");
-      menuElements[1].classList.remove("hover:bg-slate-800");
-      typeOfNotes = 'list'
-    }
-  }
+  });
+
 });
 
 createNewElement.addEventListener("click", function () {
